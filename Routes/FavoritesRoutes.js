81971854360c4be8cb6469e1cca4f3fa6fd787e0
Aditya Patel <adityakr.edugaon@gmail.com>
2026-05-14 13:44:28 +0530

    middleWare error fixed

--- Routes/FavoritesRoutes.js
+++ Routes/FavoritesRoutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const verifyToken =
-require("../MiddleWare/AuthMiddleWare");
+require("../middleware/AuthMiddleWare");
 
 const {
 
